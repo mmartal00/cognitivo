bebf8fc8af2fa72eaa2ce2916af57448a7189f8c
Mmartal00 <mmartal2100@gmail.com>
2021-10-22 13:24:50 -0500

    results

--- Vision.py
+++ Vision.py
@@ -3,10 +3,10 @@ from pprint import pprint
 
 skey = 'fa5d8e166b8444c082e139694ae140b7'
 endpoint = 'https://southcentralus.api.cognitive.microsoft.com/'
-vision_url = endpoint + "/vision/v3.2/analyze?visualFeatures=Faces&details=Landmarks"
+vision_url = endpoint + "/vision/v3.2/analyze?visualFeatures=Faces"
 
 documents ={
-    "url":"https://clutchpoints.com/wp-content/uploads/2020/07/NBA-legends-best-final-games.jpg"
+    "url":"https://www.nombres.pro/wp-content/uploads/2015/10/Nombre-para-grupos-de-amigos.jpg"
 }
 
 _headers = {"Ocp-Apim-Subscription-Key": skey}
@@ -14,6 +14,7 @@ _response=requests.post(vision_url, headers=_headers, json=documents)
 rostros = _response.json()
 
 # pprint(rostros)
+
 # print(rostros['faces'][0]['age'])
 # print(rostros['faces'][0]['gender'])
 
@@ -23,7 +24,26 @@ rostros = _response.json()
 # print(rostros['faces'][2]['age'])
 # print(rostros['faces'][2]['gender'])
 
+tot_hombres = 0
+tot_mujeres = 0
+
+tot_edad = 0
+
 for rostro in rostros['faces']:
-    pprint(rostro)
-    print(rostro['age'])
-    print(rostro['gender'])
+    rostro['gender']
+    rostro['age']
+
+    if (rostro['gender'] == 'Male'):
+        tot_hombres += 1
+    else:
+        tot_mujeres += 1
+
+print(tot_hombres)
+print(tot_mujeres)
+
+for rostro in rostros['faces']:
+    tot_edad = tot_edad + rostro['age']
+
+prom_edad = tot_edad / len(rostros['faces'])
+
+print(int(prom_edad))
